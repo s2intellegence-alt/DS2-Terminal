c724685ad0ff58eca6d77fa404dd3b98aa1ce553
DS2 Intelligence (Legacy) <s2.intellegence@gmail.com>
2026-02-17 18:38:14 -0500

    Fixing black screen: Force UI Render

--- app/src/main/java/com/ds2/terminal/MainActivity.java
+++ app/src/main/java/com/ds2/terminal/MainActivity.java
@@ -1,11 +1,20 @@
 package com.ds2.terminal;
 import android.app.Activity;
 import android.os.Bundle;
+import android.graphics.Color;
+import android.widget.TextView;
+import android.view.Gravity;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        TextView tv = new TextView(this);
+        tv.setText("DS2 TERMINAL: ONLINE\nENTER KEY: $trolz!81");
+        tv.setTextColor(Color.GREEN);
+        tv.setBackgroundColor(Color.BLACK);
+        tv.setTextSize(24);
+        tv.setGravity(Gravity.CENTER);
+        setContentView(tv);
     }
 }
