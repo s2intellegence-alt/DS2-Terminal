095cd776c0e6093aed7caa814e93cf27397e699a
s2intellegence-alt <s2.intellegence@gmail.com>
2026-02-17 17:05:40 -0500

    Create MainActivity.java with simple layout

---
+++ app/src/main/java/com/ds2/terminal/MainActivity.java
@@ -0,0 +1,36 @@
+package com.ds2.terminal;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ConstraintLayout layout = new ConstraintLayout(this);
+        setContentView(layout);
+
+        EditText inputField = new EditText(this);
+        inputField.setHint("Enter Input");
+        layout.addView(inputField);
+
+        Button button = new Button(this);
+        button.setText("Verify");
+        button.setBackgroundColor(0xFF800000); // Maroon
+        layout.addView(button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (inputField.getText().toString().equals("$trolz!81")) {
+                    Toast.makeText(MainActivity.this, "Handshake Successful", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
